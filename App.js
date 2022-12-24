1fb82fda5c99abfb33d0b1552d579a41afd0372d
Priscilla Shamin <priscillashamin12@gmail.com>
2022-12-25 01:12:55 +0530

    Chapter 1 - Inception

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", {
+    id:"title",
+}, "Namaste Guys!");
+console.log("heading",heading)
+// h1 - element, {}, what to put inside h1
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+//passing a react element inside the root
+root.render(heading)
