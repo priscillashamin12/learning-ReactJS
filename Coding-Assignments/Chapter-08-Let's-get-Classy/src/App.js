1d54556b065c0832e25a080facb0cc0f2af93570
Priscilla Shamin <priscillashamin12@gmail.com>
2023-01-28 17:36:45 +0530

    React JS & Tailwind CSS

---
+++ Coding-Assignments/Chapter-08-Let's-get-Classy/src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestrauntMenu";
+import Profile from "./components/Profile";
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/about", // parentPath/{path} => localhost:1244/about
+        element: <About />,
+        children: [
+          {
+            path: "profile", // parentPath/{path} => localhost:1244/about/profile
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
